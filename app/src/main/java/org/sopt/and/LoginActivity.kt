6b76ae5eb52f9c1969eb17842bbd90d7dac15039
hwiwon <hwhwhw74@naver.com>
2024-10-09 21:45:37 +0900

    [feat/#1] 회원가입 기능 구현

--- app/src/main/java/org/sopt/and/LoginActivity.kt
+++ app/src/main/java/org/sopt/and/LoginActivity.kt
@@ -13,14 +13,35 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import org.sopt.and.ui.theme.ANDANDROIDTheme
 
-class MainActivity : ComponentActivity() {
+class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ANDANDROIDTheme {
-                SignUp(application)
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ANDANDROIDTheme {
+        Greeting("Android")
     }
 }
--- app/src/main/java/org/sopt/and/SignUpActivity.kt
+++ app/src/main/java/org/sopt/and/SignUpActivity.kt
@@ -1,6 +1,10 @@
 package org.sopt.and
 
+import android.app.Application
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,7 +16,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -32,7 +35,6 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.sopt.and.ui.theme.ANDANDROIDTheme
@@ -43,7 +45,7 @@ class SignUpActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ANDANDROIDTheme {
-                SignUp()
+                SignUp(application)
             }
         }
     }
@@ -76,39 +78,40 @@ fun SignUpHeader() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
-fun SignUpForm() {
-    var emailId by remember { mutableStateOf("") }
+fun SignUpForm(application: Application) {
+    var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var isPasswordVisible by remember { mutableStateOf(false) }
     Column(
         modifier = Modifier
             .background(Color.Black)
-            .padding(start = 15.dp, top = 30.dp, end = 15.dp)
             .fillMaxWidth()
     ) {
         val text = buildAnnotatedString {
-            withStyle(style = SpanStyle(fontWeight = FontWeight.Bold))
+            withStyle(style = SpanStyle(fontWeight = FontWeight.Medium))
             { append("이메일과 비밀번호") }
             withStyle(style = SpanStyle(fontWeight = FontWeight.ExtraLight))
             { append("만으로\n") }
-            withStyle(style = SpanStyle(fontWeight = FontWeight.Bold))
+            withStyle(style = SpanStyle(fontWeight = FontWeight.Medium))
             { append("Wavve를 즐길 수 ") }
             withStyle(style = SpanStyle(fontWeight = FontWeight.ExtraLight))
             { append("있어요!") }
         }
         Text(
+            modifier = Modifier.padding(start = 15.dp, top = 30.dp, end = 15.dp),
             text = text,
-            fontSize = 25.sp,
+            fontSize = 28.sp,
             color = Color.White,
         )
         Spacer(modifier = Modifier.height(30.dp))
         TextField(
             modifier = Modifier
-                .fillMaxWidth(),
-            value = emailId,
-            onValueChange = { emailId = it },
+                .fillMaxWidth()
+                .padding(start = 15.dp, top = 30.dp, end = 15.dp),
+            value = email,
+            onValueChange = { email = it },
             label = {
                 Text(text = "wavve@example.com", color = Color.DarkGray)
             },
@@ -120,15 +123,17 @@ fun SignUpForm() {
         )
         Text(
             modifier = Modifier
-                .padding(15.dp),
+                .padding(start = 15.dp, top = 15.dp, end = 15.dp),
             text = "⚠️ 로그인, 비밀번호 찾기, 알림에 사용되니 정확한 이메일을 입력해 주세요.",
             fontSize = 10.sp,
             color = Color.Gray
         )
         Spacer(modifier = Modifier.height(10.dp))
-        Box() {
+        Box {
             TextField(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 15.dp, top = 20.dp, end = 15.dp),
                 value = password,
                 onValueChange = { password = it },
                 label = {
@@ -139,13 +144,14 @@ fun SignUpForm() {
                     focusedContainerColor = Color.Gray,
                     unfocusedContainerColor = Color.Gray
                 ),
+                placeholder = { Text("ex) abcdEFG123") },
                 visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation()
             )
             TextButton(
                 onClick = { isPasswordVisible = !isPasswordVisible },
                 modifier = Modifier
                     .align(Alignment.CenterEnd)
-                    .padding(end = 8.dp)
+                    .padding(top=20.dp, end=8.dp)
                     .background(Color.White.copy(alpha = 0f))
             ) {
                 Text(
@@ -157,39 +163,77 @@ fun SignUpForm() {
         }
         Text(
             modifier = Modifier
-                .padding(15.dp),
+                .padding(start = 15.dp, top = 15.dp, end = 15.dp),
             text = "⚠️ 비밀번호는 8~20자 이내로 영문 대소문자, 숫자, 특수문자 중 3가지 이상 혼용하여 입력해 주세요.",
             fontSize = 10.sp,
             color = Color.Gray
         )
+        Spacer(modifier = Modifier.weight(1f))
+        TextButton(
+            onClick = {
+                if (validateSignUp(email = email, password = password)) {
+                    val intent = Intent(application, LoginActivity::class.java)
+                    intent.apply{
+                        flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        putExtra("email",email)
+                        putExtra("password",password)
+                    }
+                    application.startActivity(intent)
+                } else {
+                    Toast.makeText(application, "회원가입 조건에 부합하지 않습니다.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Gray.copy(alpha = 0.5f))
+                .padding(top = 15.dp, bottom = 15.dp),
+        ) {
+            Text(
+                text = "Wavve 회원가입",
+                fontSize = 20.sp,
+                color = Color.White,
+                textAlign = TextAlign.Center
+            )
+        }
     }
+
+}
+
+
+fun validateSignUp(email: String, password: String): Boolean {
+    val emailRegex = Regex("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")
+    if (!emailRegex.matches(email)) {
+        return false
+    }
+    if (password.length < 8 || password.length > 20) {
+        return false
+    }
+    val lowerCaseRegex = Regex("[a-z]")
+    val upperCaseRegex = Regex("[A-Z]")
+    val digitRegex = Regex("[0-9]")
+    val specialRegex = Regex("[!@#\$%^&*(),.?\\\":{}|<>]")
+
+    val hasLowerCase = lowerCaseRegex.containsMatchIn(password)
+    val hasUpperCase = upperCaseRegex.containsMatchIn(password)
+    val hasDigitCase = digitRegex.containsMatchIn(password)
+    val hasSpecialChar = specialRegex.containsMatchIn(password)
+
+    val criteriaCnt = listOf(hasLowerCase, hasUpperCase, hasDigitCase, hasSpecialChar).count { it }
+
+    return criteriaCnt >= 3
 }
 
 @Composable
-fun SignUpBottom(){
-    Text(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(Color.Gray.copy(alpha = 0.5f))
-            .padding(top = 15.dp, bottom = 15.dp),
-        text = "Wavve 회원가입",
-        fontSize = 20.sp,
-        color = Color.White,
-        textAlign = TextAlign.Center
-    )
-}
+fun SignUp(application: Application) {
 
-@Preview
-@Composable
-fun SignUp() {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
     ) {
         SignUpHeader()
-        SignUpForm()
-        Spacer(modifier = Modifier.weight(1f))
-        SignUpBottom()
+        SignUpForm(application)
     }
 }
