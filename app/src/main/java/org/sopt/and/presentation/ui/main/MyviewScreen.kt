34569ea6336faaa2b0770e36b81445f9059d7968
hwiwon <hwhwhw74@naver.com>
2024-11-01 23:05:51 +0900

    [refactor/#5] my뷰 시청내역 컴포넌트

--- app/src/main/java/org/sopt/and/presentation/ui/main/MyviewScreen.kt
+++ app/src/main/java/org/sopt/and/presentation/ui/main/MyviewScreen.kt
@@ -3,6 +3,7 @@ package org.sopt.and.presentation.ui.main
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -16,7 +17,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.sopt.and.R
@@ -71,61 +71,13 @@ fun MyviewScreen(signUpViewModel: SignUpViewModel) {
         }
         MyPurchaseInfoCol(mainText = stringResource(R.string.my_1stPurchase_description))
         MyPurchaseInfoCol(mainText = stringResource(R.string.my_NowTicket_description))
-        Box(
+        Column(
             modifier = Modifier
+                .fillMaxHeight()
                 .background(Color.Black)
-                .padding(20.dp)
-                .fillMaxWidth()
         ) {
-            Text(
-                text = "전체 시청내역",
-                color = Color.White,
-                fontWeight = FontWeight.W800,
-                fontSize = 23.sp
-            )
-            Text(
-                text = "⚠️",
-                fontSize = 50.sp,
-                modifier = Modifier
-                    .align(Alignment.TopCenter)
-                    .padding(top = 90.dp)
-            )
-            Text(
-                text = "시청내역이 없어요.",
-                fontSize = 15.sp,
-                color = Color.Gray,
-                modifier = Modifier
-                    .align(Alignment.TopCenter)
-                    .padding(top = 170.dp)
-            )
-        }
-        Box(
-            modifier = Modifier
-                .background(Color.Black)
-                .padding(20.dp)
-                .fillMaxSize()
-        ) {
-            Text(
-                text = "관심 프로그램",
-                color = Color.White,
-                fontWeight = FontWeight.W800,
-                fontSize = 23.sp
-            )
-            Text(
-                text = "⚠️",
-                fontSize = 50.sp,
-                modifier = Modifier
-                    .align(Alignment.TopCenter)
-                    .padding(top = 90.dp)
-            )
-            Text(
-                text = "관심 프로그램이 없어요.",
-                fontSize = 15.sp,
-                color = Color.Gray,
-                modifier = Modifier
-                    .align(Alignment.TopCenter)
-                    .padding(top = 170.dp)
-            )
+            MyPurchaseInfoCol(title = "전체 시청내역")
+            MyPurchaseInfoCol(title = "관심 프로그램")
         }
     }
 }
